0d7d000f19fc153d47d4107b1221d6d4dfad378a
Sefton419 <austin.sefton@me.com>
2016-07-24 16:48:24 -0700

    [add] solves add two numbers leetcode toyproblem

--- Toy-Problems/Add-Two-Numbers.js
+++ Toy-Problems/Add-Two-Numbers.js
@@ -9,6 +9,8 @@ You are given two linked lists representing two non-negative numbers. The digits
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 
+Output and inputs must be in the form of a linked list
+
 */
 
 
@@ -26,62 +28,85 @@ Output: 7 -> 0 -> 8
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
 
-    // instantiate new linked list
+ var addTwoNumbers = function(l1, l2) {
 
-    function List() {
-    	this.head = null;
-    	this.tail = null
-    }
+ // instantiate new linked list
 
-    List.prototype.ListNode = function (val){
-        this.val = val;
-        this.next = null;
-    }
+     function List() {
+         this.head = null;
+         this.tail = null
+     };
 
-    List.prototype.addNode = function (val) {
+     List.prototype.ListNode = function (val){
+         this.val = val;
+         this.next = null;
+     };
 
-	    let node = new this.ListNode(val);
+     List.prototype.addNode = function (val) {
+         let node = new this.ListNode(val);
+         if (this.head === null && this.tail === null) {
+             this.head = node;
+             this.tail = node;
+         } else {
+             this.tail.next = node;
+             this.tail = node;
+         }
 
-	    if (this.head === null && this.tail === null) {
-	    	this.head = node;
-	    	this.tail = node;
-	    } else {
-	    	this.tail.next = node;
-	    	this.tail = node;
-	    }
+     };
 
-    }
+     let l3 = new List;
 
-    let l3 = new List;
+     let numbers = [];
 
-    // recurse function with parameters of both linked lists
-    function recurse (list1, list2) {
+     let tempNumber = '';
 
-        // if list1.next === null
+     // Create the complete numbers to add
+     function materializeNumber (list) {
+         // if next === null
+         if (list === null)
+             // don't recurse
+             return;
 
-        if (l3 === null) {
-            l3 = new ListNode(list1.val + list2.val);
-        } else {
-            // take list1.val and add to list2.val
-            l3.addNode(list1.val + list2.val);
-        }
+         // make number string, save to variable
+         tempNumber = tempNumber + String(list.val);
+         materializeNumber(list.next);
+     };
 
-        // recurse (list1.next, list2.next)
-        if (list1.next !== null) {
-        	recurse(list1.next, list2.next);
-        }
-
-    }
-
-    // call recurse (l1, l2)
-    recurse(l1, l2)
-    // NEXT, make all values in l3 single-digit
+     materializeNumber(l1);
+     numbers.push(tempNumber.split('').reverse('').join(''));
+     tempNumber = '';
+     materializeNumber(l2);
+     numbers.push(tempNumber.split('').reverse('').join(''));
 
 
+     let added = String(Number(numbers[0]) + Number(numbers[1]));
 
-    // return l3
-    return l3;
 
-};
+     function returnLinkedList (number) {
+         // create linked list with number of digits
+         let numArr = number.split('');
+
+         while (numArr.length > 0) {
+            let temp = numArr.pop();
+             l3.addNode(Number(temp));
+         }
+     };
+
+     returnLinkedList(added);
+
+     // return l3
+     return l3.head;
+
+ };
+
+/*
+
+Test case:
+
+ let one = {val: 1, next: {val: 2, next: {val: 5, next: null}}};
+ let two = {val: 6, next: {val: 7, next: {val: 1, next: null}}};
+
+ addTwoNumbers(one, two);
+
+*/
