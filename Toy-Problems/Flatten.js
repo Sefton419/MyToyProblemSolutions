3b619226d3d7d6cd58a015c6ba741a3dbc606c28
Sefton419 <austin.sefton@me.com>
2016-08-01 15:40:53 -0700

    [add] solves flatten from challenge app

---
+++ Toy-Problems/Flatten.js
@@ -0,0 +1,31 @@
+/*
+
+Flatten
+
+Given an array of arrays, return all elements in a single array. You must use recursion.
+
+*/
+
+
+
+function flatten (array) {
+  let results = [];
+  // recurse (arr)
+  function recurse (arr) {
+    // iterate through array
+    for (var i = 0; i < arr.length; i++) {
+      let val = arr[i];
+      // if value is array
+      if (Array.isArray(val)) {
+        // recurse
+        recurse(val);
+      } else {
+      // if is not array
+        // push result to results
+        results.push(val);
+      }
+    }
+  }
+  recurse (array);
+  return results;
+};
