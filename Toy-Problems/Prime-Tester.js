269d97dc010d6d152056dd2116e3ee974b2bd320
Sefton419 <austin.sefton@gmail.com>
2016-09-21 13:09:16 -0700

    [edit] changes syntax of prime tester

--- Toy-Problems/Prime-Tester.js
+++ Toy-Problems/Prime-Tester.js
@@ -2,6 +2,7 @@
 /*
 
 Prime Tester
+
 A prime number is an integer greater than 1 that has no divisors other than itself and 1. Write a function that accepts a number and returns true if it’s a prime number, false if it’s not. The grader is looking at the efficiency of your solution (number of operations) as well as correctness! 
 
 */
