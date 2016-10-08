8520e77b8b8e846300e0c269201d8f6236c503df
Sefton419 <austin.sefton@gmail.com>
2016-10-07 17:59:51 -0700

    [add] solves reverse words algorithm and edits file name of playing with passphrases

---
+++ Toy-Problems/Reverse-Words.js
@@ -0,0 +1,18 @@
+/*
+
+Reverse Words
+
+Write a reverseWords function that accepts a string a parameter, and reverses each word in the string. Every space should stay, so you cannot use words from Prelude.
+
+Example:
+
+reverseWords("This is an example!"); 
+// returns  "sihT si na !elpmaxe"
+
+*/
+
+function reverseWords(str) {
+  return str.split(' ').map(word => {
+    return word.split('').reverse().join('');
+  }).join(' ');
+};
