206fde948510b929bae45ee2abb561d7668c94da
Sefton419 <austin.sefton@gmail.com>
2016-12-15 17:24:49 -0800

    [add] triangle algorithm started, must approach with recursion

---
+++ Toy-Problems/Triangle.js
@@ -0,0 +1,13 @@
+/*
+
+Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+
+For example, given the following triangle
+
+*/
+
+
+
+var minimumTotal = function(triangle) {
+
+};
