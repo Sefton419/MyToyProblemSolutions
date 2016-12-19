f7cb3053200f323e01279f149bd2185ddd6a0733
Sefton419 <austin.sefton@gmail.com>
2016-12-18 20:36:02 -0800

    [edit] changes experimental syntax back to basic solution for fibonacci algorithm

--- Toy-Problems/Fibonacci.js
+++ Toy-Problems/Fibonacci.js
@@ -4,12 +4,12 @@ Create a recursive fibonacci function that returns the nth integer
 
 */
 
-let result = [];
+
 let fibonacci = (n) => {
 	if (n <= 1) {
-		return result.push(1);
+		return 1;
 	} else {
-		return result.push(fibonacci(n - 2) + fibonacci(n - 1));
+		return fibonacci(n - 1) + fibonacci(n - 2);
 	}
 };
 
